edb42dbefa465b8060c75024bba582324ff752b7
Quang Minh Pham <quangminh1575@gmail.com>
2023-03-03 12:04:06 +0100

    test success !

--- clock.js
+++ clock.js
@@ -1,7 +1,7 @@
-const hourEl = document.getElementById("hour");
-const minuteEl = document.getElementById("minutes");
-const secondEl = document.getElementById("seconds");
-const ampmEl = document.getElementById("ampm");
+const hourEl = document.getElementById('hour');
+const minuteEl = document.getElementById('minutes');
+const secondEl = document.getElementById('seconds');
+const ampmEl = document.getElementById('ampm');
 
 function updateClock() {
   let h = new Date().getHours();
@@ -11,19 +11,19 @@ function updateClock() {
 
   if (h > 12) {
     h -= 12;
-    ampm = "PM";
+    ampm = 'PM';
   }
 
   h = h < 10 ? `0${h}` : h;
-         m = m < 10 ? `0${m}` : m;
+  m = m < 10 ? `0${m}` : m;
   s = s < 10 ? `0${s}` : s;
 
-       hourEl.innerText = h;
-       minuteEl.innerText = m;
+  hourEl.innerText = h;
+  minuteEl.innerText = m;
   secondEl.innerText = s;
-       ampmEl.innerText = ampm;
+  ampmEl.innerText = ampm;
   setTimeout(() => {
-           updateClock();
+    updateClock();
   }, 1000);
 }
 
