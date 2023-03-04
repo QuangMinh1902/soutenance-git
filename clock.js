1e4ea9c2b24ac85c9f1acc3f3ced143b8897f63e
Quang Minh Pham <quangminh1575@gmail.com>
2023-03-04 09:57:29 +0100

    success !

--- clock.js
+++ clock.js
@@ -1,12 +1,12 @@
-const hourEl = document.getElementById("hour");
-const minuteEl = document.getElementById("minutes");
-const secondEl = document.getElementById("seconds");
+const hourEl = document.getElementById('hour');
+const minuteEl = document.getElementById('minutes');
+const secondEl = document.getElementById('seconds');
 const ampmEl = document.getElementById('ampm');
 
 function updateClock() {
   let h = new Date().getHours();
   let m = new Date().getMinutes();
-                            let s = new Date().getSeconds();
+  let s = new Date().getSeconds();
   let ampm = 'AM';
 
   if (h > 12) {
@@ -14,14 +14,14 @@ function updateClock() {
     ampm = 'PM';
   }
 
-                        h = h < 10 ? `0${h}` : h;
-                       m = m < 10 ? `0${m}` : m;
+  h = h < 10 ? `0${h}` : h;
+  m = m < 10 ? `0${m}` : m;
   s = s < 10 ? `0${s}` : s;
 
   hourEl.innerText = h;
   minuteEl.innerText = m;
-                    secondEl.innerText = s;
-                          ampmEl.innerText = ampm;
+  secondEl.innerText = s;
+  ampmEl.innerText = ampm;
   setTimeout(() => {
     updateClock();
   }, 1000);
--- countdown.js
+++ countdown.js
@@ -1,9 +1,9 @@
-const dayEl = document.getElementById("day");
-const hourEl = document.getElementById("hour");
-const minuteEl = document.getElementById("minute");
-const secondEl = document.getElementById("second");
+const dayEl = document.getElementById('day');
+const hourEl = document.getElementById('hour');
+const minuteEl = document.getElementById('minute');
+const secondEl = document.getElementById('second');
 
-const newYearTime = new Date("Jan 1, 2024 00:00:00").getTime();
+const newYearTime = new Date('Jan 1, 2024 00:00:00').getTime();
 
 function updateCountdown() {
   const now = new Date().getTime();
